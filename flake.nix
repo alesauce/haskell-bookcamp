166a766cb7ca7af6f228c9dbe73204b73ba385d8
Alexander Sauceda <alexander@alexandersauceda.dev>
2024-08-07 21:30:03 -0700

    init

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Dev environment setup for a new Haskell project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      allSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            ghc
+            haskellPackages.stack
+            haskellPackages.hlint
+            haskellPackages.ormolu
+            haskellPackages.hasktags
+          ];
+        };
+      });
+    };
+}
